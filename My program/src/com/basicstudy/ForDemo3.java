b6b8635fb9ca57f7913576a65f62afab75486002
yanpanghong <1608012444@qq.com>
2020-09-26 20:51:04 +0800

    9.26

---
+++ My program/src/com/basicstudy/ForDemo3.java
@@ -0,0 +1,11 @@
+package com.basicstudy;
+
+public class ForDemo3 {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i=1;i<=5;i++){
+            sum +=i;
+        }
+        System.out.println("1-5的总和为:"+sum);
+    }
+}
