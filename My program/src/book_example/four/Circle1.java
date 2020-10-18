7d081ec4ed368fa5fc5edf2240ff8aaeea0582fd
yanpanghong <1608012444@qq.com>
2020-10-18 15:41:55 +0800

    对象的组合

---
+++ My program/src/book_example/four/Circle1.java
@@ -0,0 +1,32 @@
+package book_example.four;
+
+/**
+ * @ClassName Circle1
+ * @Description TODO
+ * @Author YPH
+ * @Date 2020/10/18
+ **/
+
+public class Circle1 {
+    double x,y,radius;
+    public void setX(double a){
+        x=a;
+    }
+    public double getX(){
+        return x;
+    }
+    public void setY(double b){
+        y=b;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setRadius(double r){
+        if (r>0){
+            r=radius;
+        }
+    }
+    public double getRadius(){
+        return radius;
+    }
+}
