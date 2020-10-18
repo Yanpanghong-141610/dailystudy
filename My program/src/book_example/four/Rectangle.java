60fa14ebd16eb026cf7b414d244c11104b41081f
yanpanghong <1608012444@qq.com>
2020-10-18 15:41:18 +0800

    对象的组合

---
+++ My program/src/book_example/four/Rectangle.java
@@ -0,0 +1,41 @@
+package book_example.four;
+
+/**
+ * @ClassName Rectangle
+ * @Description TODO
+ * @Author YPH
+ * @Date 2020/10/18
+ **/
+
+public class Rectangle {
+    double x,y,width,height;
+    public void setX(double a)
+    {
+        x=a;
+    }
+    public double getX(){
+        return x;
+    }
+    public void setY(double b){
+        y=b;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setWidth(double w){
+        if (w>0){
+            width=w;
+        }
+    }
+    public double getWidth(){
+        return width;
+    }
+    public void setHeight(double h){
+        if (height>0){
+            height=h;
+        }
+    }
+    public double getHeight(){
+        return height;
+    }
+}
