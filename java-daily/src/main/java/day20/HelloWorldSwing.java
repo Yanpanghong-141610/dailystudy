92a7a703e6f4c0b6d678058ea5784e68f5358ed5
yanpanghong <1608012444@qq.com>
2020-11-08 21:12:33 +0800

    day20

--- java-daily/src/main/java/day20/HelloWorldSwing.java
+++ java-daily/src/main/java/day20/HelloWorldSwing.java
@@ -1,10 +1,36 @@
 package day20;
+
+import javax.swing.*;
+
 /**
-  *@ClassName HelloWorldSwing
-  *@Description HelloWorld窗体程序
-  *@Author YPH
-  *@Date 2020/11/8
-**/
+ * @ClassName HelloWorldSwing
+ * @Description HelloWorld窗体程序
+ * 创建并显示GUI。出于线程安全的考虑，
+ * 这个方法在事件调用线程中调用
+ * @Author YPH
+ * @Date 2020/11/8
+ **/
 
 public class HelloWorldSwing {
+    private static void createAndShowGUI(){
+        //确保一个漂亮的外观风格
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //创建及设置窗口
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //添加"Hello World"标签
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //显示窗口
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //显示应用GUI
+        javax.swing.SwingUtilities.invokeLater(HelloWorldSwing::createAndShowGUI);
+    }
 }
--- java-daily/src/main/java/day20/SwingLoginExample.java
+++ java-daily/src/main/java/day20/SwingLoginExample.java
@@ -1,10 +1,80 @@
 package day20;
+
+import javax.swing.*;
+
 /**
-  *@ClassName SwingLoginExample
-  *@Description 一个用户登录框实例
-  *@Author YPH
-  *@Date 2020/11/8
-**/
+ * @ClassName SwingLoginExample
+ * @Description 一个用户登录框实例
+ * @Author YPH
+ * @Date 2020/11/8
+ **/
 
 public class SwingLoginExample {
+    public static void main(String[] args) {
+        //创建 JFrame 实例
+        JFrame frame = new JFrame("Login Example");
+        //设置窗体的宽和高
+        frame.setSize(350,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /**
+         * 创建面板,这个类似于 HTML 的 div 标签
+         * 我们可以创建多个面板并在 JFrame 中指定位置
+         * 面板中我们可以添加文本字段，按钮及其他组件
+         */
+        JPanel panel = new JPanel();
+        //添加面板
+        frame.add(panel);
+        /**
+         * 调用用户定义的方法并添加组件到面板
+         */
+        placeComponents(panel);
+
+        //设置界面可见
+        frame.setVisible(true);
+    }
+    private static void placeComponents(JPanel panel){
+
+        /**
+         * 这边设置布局为 null
+         */
+        panel.setLayout(null);
+
+        //创建JLabel
+        JLabel userLabel = new JLabel("User:");
+
+        /**
+         * 这个方法定义了组件的位置
+         * setBounds(x,y,width,height)
+         * x和y指定左上角的新位置，由width 和 height 指定新的大小
+         */
+        userLabel.setBounds(10,20,80,25);
+        panel.add(userLabel);
+
+        /**
+         * 创建文本域用于用户输入
+         */
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+        //输入密码的文本域
+        JLabel passworldLabel = new JLabel("password:");
+        passworldLabel.setBounds(10,50,80,25);
+        panel.add(passworldLabel);
+
+        /**
+         * 这个类似用于输入的文本域
+         * 但是输入的信息会以点号代替，用于包含密码的安全性
+         */
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100,50,165,25);
+        panel.add(passwordText);
+
+        //创建登录按钮
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10,80,80,25);
+        panel.add(loginButton);
+
+    }
 }
