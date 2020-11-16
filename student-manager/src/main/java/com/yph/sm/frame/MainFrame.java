c6b80648e496c6a74185a0a287233fe495f641f1
yanpanghong <1608012444@qq.com>
2020-11-16 20:23:58 +0800

    主页面

--- student-manager/src/main/java/com/yph/sm/frame/MainFrame.java
+++ student-manager/src/main/java/com/yph/sm/frame/MainFrame.java
@@ -1,6 +1,7 @@
 package com.yph.sm.frame;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @ClassName MainFrame
@@ -11,9 +12,42 @@ import javax.swing.*;
 
 public class MainFrame extends JFrame {
     private JPanel mainPanel;
+    private JPanel topPanel;
+    private JPanel centerPanel;
+    private JButton 院系管理Button;
+    private JButton 班级管理Button;
+    private JButton 学生管理Button;
+    private JButton 奖惩管理Button;
+    private JPanel departmentPanel;
+    private JPanel classPanel;
+    private JPanel studentPanel;
+    private JPanel rewardPanel;
+    private final CardLayout c;
 
     public MainFrame(){
-        this.setTitle("MainFrame");
+        init();
+        c=new CardLayout();
+        centerPanel.setLayout(c);
+        centerPanel.add("1",departmentPanel);
+        centerPanel.add("2",classPanel);
+        centerPanel.add("3",studentPanel);
+        centerPanel.add("4",rewardPanel);
+
+        院系管理Button.addActionListener(e ->{
+            c.show(centerPanel,"1");
+        });
+        班级管理Button.addActionListener(e ->{
+            c.show(centerPanel,"2");
+        });
+        学生管理Button.addActionListener(e ->{
+            c.show(centerPanel,"3");
+        });
+        奖惩管理Button.addActionListener(e ->{
+            c.show(centerPanel,"4");
+        });
+    }
+
+    private void init() {
         this.setContentPane(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
